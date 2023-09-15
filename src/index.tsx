17f120faf95e6d4ba7788f3e686ca690e5be1e57
DanyPlaya <danila.lusuy@gmail.com>
2023-09-15 17:42:39 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import {ChakraProvider} from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import App from './app/App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
+)
